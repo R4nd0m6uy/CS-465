58aafb3915946463507855256ef49131ca7a7ecd
Logan Pullen <loganpullen@MacBook-Pro.local>
2026-05-22 20:38:19 -0400

    Complete Module 3 JSON templating

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,11 @@
+const fs = require('fs');
+
+const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
 const travel = (req, res) => {
   res.render('travel', {
-    title: 'Travlr Getaways'
+    title: 'Travlr Getaways',
+    trips
   });
 };
 
